6fadfad2d42d0ae8e485eea6c29528139cccda72
Javier Leguizamo <jlegui745@gmail.com>
2024-02-14 06:01:13 -0500

    Changed electron-forge config

--- forge.config.js
+++ forge.config.js
@@ -1,16 +1,18 @@
+const pkg = require('./package.json')
+
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './test_icon.ico',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      config: {
+        exe: `${pkg.readableName}.exe`,
+        setupExe: `${pkg.readableName} v${pkg.version} Setup.exe`,
+      },
     },
   ],
   plugins: [
