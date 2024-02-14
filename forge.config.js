02fbcbe201b5214b5af81be8857624e16a6ebe86
Javier Leguizamo <jlegui745@gmail.com>
2024-02-14 04:46:36 -0500

    Backed up a bit the sudden version changes

---
+++ forge.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+}
