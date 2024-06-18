e7eb4d4148e896a28b547193cb8837d79c7596a3
Javier Leguizamo <jlegui745@gmail.com>
2024-06-18 01:01:49 -0500

    Misc changes

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -115,49 +115,47 @@ window.ffmpeg.onFinish(function ({ pos, length }) {
   }, 3000)
 })
 
-const drag_container = document.querySelector("#drag-files");
+const drag_container = document.querySelector('#drag-files')
 
 drag_container.addEventListener('drop', async (event) => {
-  event.preventDefault();
-  event.stopPropagation();
+  event.preventDefault()
+  event.stopPropagation()
 
-  let files = [];
-  let extensions = ['mkv', 'avi', 'mp4'];
+  let files = []
+  let extensions = ['mkv', 'avi', 'mp4']
 
   for (const file of event.dataTransfer.files) {
-    let valid = false;
-    extensions.forEach(extension => {
-      if (file.path.endsWith(extension)) valid = true;
-    });
+    let valid = false
+    extensions.forEach((extension) => {
+      if (file.path.endsWith(extension)) valid = true
+    })
 
-    if (valid) files.push(file.path);
+    if (valid) files.push(file.path)
   }
 
-  if (files.length === 0) return leftDrop();
+  if (files.length === 0) return leftDrop()
 
   compressBtn.disabled = false
   abortBtn.disabled = true
 
   selectVideosBtn.value = files
 
-  window.logger.debug("renderer", { files });
-  leftDrop();
+  window.logger.debug('renderer', { files })
+  leftDrop()
   await window.app.save(settings)
-});
+})
 
 drag_container.addEventListener('dragover', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-});
+  e.preventDefault()
+  e.stopPropagation()
+})
 
 document.addEventListener('dragenter', (event) => {
-  window.logger.status("renderer", "File(s) are being dragged")
-  drag_container.classList.add("active");
-});
+  window.logger.status('renderer', 'File(s) are being dragged')
+  drag_container.classList.add('active')
+})
 
-drag_container.addEventListener('dragleave', leftDrop);
-
-function leftDrop() {
-  window.logger.status("renderer", "File(s) left drop area")
-  drag_container.classList.remove("active");
-}
+document.addEventListener('dragleave', (event) => {
+  window.logger.status('renderer', 'File(s) left drop area')
+  drag_container.classList.remove('active')
+})
